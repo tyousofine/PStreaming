572475bddda553e5eca9b3f6a540426e8508e465
tyousofine <group8info3174@gmail.com>
2023-11-28 15:41:27 -0500

    add chart2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 // Open page
 function openPage(id) {
     const containerList = document.querySelectorAll('.contentContainer');
-    console.log(containerList)
+
     //Remove an active class If an element has an active class
     for (let i = 0; i < containerList.length; i++) {
         if (containerList[i].classList.contains('active')) {
@@ -29,15 +29,13 @@ function toggleHamburgerFunction() {
     if (!hamburger.classList.contains('active')) {
         hamburger.classList.add('active')
         navbar.classList.add('nav-hamburger');
-        console.log('Hamburger class: ', hamburger.classList)
-        console.log('navbar class: ', navbar.classList)
+
     }
 
     else {
         hamburger.classList.remove('active')
         navbar.classList.remove('nav-hamburger')
-        console.log('Hamburger class: ', hamburger.classList)
-        console.log('navbar class: ', navbar.classList)
+
 
     }
 
@@ -62,12 +60,15 @@ function openNoti(evt, notiKind) {
 // DASHBOARD SECTION
 // *****************
 
-
-// Chart1 - bar chart - Proportion of Channels to Categories
-let svgWidth = 500; // 600 originally - tt
-let bSvgHeight = 400; // 600 originally - tt
+// Global variables for all charts
+let width = 500;
+let height = 400;
 let padding = 80;
 
+
+//********************************** */
+// CHART 1 
+//********************************** */
 
 // create svg for chart 1
 let chart1Svg = d3
@@ -76,14 +77,14 @@ let chart1Svg = d3
     .attr("width", "100%")
     .attr("height", "100%")
     .attr('preserveAspectRatio', 'xMinYMin meet')
-    .attr('viewBox', '0 0 500 400')
+    .attr('viewBox', '0 -20 500 400')
     .attr("style", "background-color: #2c2b2b")
     .style("box-shadow", "0 0 15px 2px #c0c0c055")
     .style("border-radius", "4px")
 
 // set width and height for chart svg content
-let bInner_width = svgWidth - padding;
-let bInner_height = bSvgHeight - padding - 20;
+let bInner_width = width - padding;
+let bInner_height = height - padding - 20;
 
 let bG = chart1Svg
     .append("g")
@@ -95,8 +96,8 @@ bG
     .append("text")
     .attr("text-anchor", "center")
     .attr("x", bInner_width / 8)
-    .attr("y", 20)
-    .text("Proportion of YouTubers per Category")
+    .attr("y", 0)
+    .text("Percentage of YouTubers per Category")
     .style("fill", "#fff")
     .style("font-size", 17)
     .style("letter-spacing", 0.8)
@@ -123,70 +124,47 @@ d3.csv("./data/top_100_youtubers.csv").then(function (data) {
         percentage: ((count / totalCategories) * 100).toFixed(0),
     }));
 
-    // console.log("data: ", data)
 
-    // create xScale for bar chart
+    // create scales
     let bXscale = d3
         .scaleBand()
-        .range([0, bInner_width - 30])
+        .range([-20, bInner_width - 20])
+        .paddingOuter(0.3)
         .domain(data.map((d) => d.category))
 
+    let bYscale = d3.scaleLinear().range([bInner_height + 20, 40]).domain([0, 50]);
 
-    // create xAxis   
+    // create Axis'  
     let bXaxis = d3.axisBottom().scale(bXscale)
+    let bYaxis = d3.axisLeft().scale(bYscale) // add .tickFormat("") if you want numbers gone;
 
     // append  xAxis
-    const bottom = bG.append("g")
-        .attr("transform", `translate(0,${bInner_height})`)
+    const bottomAxis = bG.append("g")
+        .attr("transform", `translate(0,${bInner_height + 20})`)
         .call(bXaxis)
         .attr("color", "#c0c0c0")
 
     // add angled labels
-    bottom
+    bottomAxis
         .selectAll('.tick text')
-        .attr("transform", "rotate(-35)")
+        .attr("transform", "rotate(-25)")
         .style("text-anchor", "end")
         .style("color", "#c0c0c0")
         .style("font-family", "noto")
         .style("font-size", 9)
 
-    // create group for axis title
-    const labelGroup = bG.append("g")
-        .attr("transform", "translate(" + (bInner_width / 2.5) + "," + (bInner_height + 60) + ")");
 
-    // Add axis title
-    labelGroup.append("text")
-        .style("text-anchor", "middle")
-        .style("fill", "#fff")
-        .style("font-family", "noto")
-        .style("font-size", 15)
-        .style("letter-spacing", 1)
-        .text("Categories");
-
-
-    // Create Y scale
-    let bYscale = d3.scaleLinear().range([bInner_height, 0]).domain([0, 100]).range([300, 50]);
-
-    // append y axis
-    let bYaxis = d3.axisLeft().scale(bYscale).ticks(12);
-
-    // append label and label style
+    // append label and label style for yAxis
     bG.append("g")
+        .attr("transform", `translate(-20, 0)`)
         .call(bYaxis)
         .attr('color', "#c0c0c0")
-        .append("text")
-        .attr("class", "naming")
-        .attr("transform", "rotate(-90)")
-        .attr("x", -150)
-        .attr("y", -40)
-        .attr("fill", "#fff")
-        .text("Percentage")
-        .style("font-family", "noto")
-        .style("font-size", 13)
-        .style("letter-spacing", 2);
+        .style("font-size", 8)
+
 
     let graph = bG.selectAll(".graph").data(data).enter().append("g");
 
+    // add graph - bars
     graph
         .append("rect")
         .attr("class", "barC5")
@@ -194,11 +172,11 @@ d3.csv("./data/top_100_youtubers.csv").then(function (data) {
             return bXscale(d.category) + 10;
         })
         .attr("y", function (d) {
-            return bYscale(d.percentage) - 1;
+            return bYscale(d.percentage);
         })
         .attr("width", bXscale.bandwidth() - 20)
         .attr("height", function (d) {
-            return bInner_height - bYscale(d.percentage);
+            return 320 - bYscale(d.percentage);
         })
         .attr("fill", "#D4Af37");
 
@@ -220,6 +198,194 @@ d3.csv("./data/top_100_youtubers.csv").then(function (data) {
         .attr("class", "textChart")
         .style("font-size", 12)
 });
+
+
+
+//********************************************************************** */
+// CHART 2 - scatter plot - Relationship between Likes and subscribers
+//********************************************************************** */
+
+// create svg for chart 1
+let svgChart2 = d3
+    .select("#chart2")
+    .append("svg")
+    .attr("width", "100%")
+    .attr("height", "100%")
+    .attr('preserveAspectRatio', 'xMinYMin meet')
+    .attr('viewBox', '0 -20 500 400')
+    .attr("style", "background-color: #2c2b2b")
+    .style("box-shadow", "0 0 15px 2px #c0c0c055")
+    .style("border-radius", "4px")
+
+const scatterPlotInnerWidth = width - padding
+const scatterPlotInnerHeight = height - padding;
+
+let spG = svgChart2
+    .append("g")
+    .attr("class", "group")
+    .attr("transform", "translate(65, 10)")
+
+// add scatterplot title 
+spG
+    .append("text")
+    .attr("text-anchor", "center")
+    .attr("x", bInner_width / 8)
+    .attr("y", 0)
+    .text("Relationship Between Likes and Subs")
+    .style("fill", "#fff")
+    .style("font-size", 17)
+    .style("letter-spacing", 0.8)
+
+
+// read data 
+d3.csv("./data/top_100_youtubers.csv").then(function (data) {
+
+    // color set for circles
+    var color = d3.scaleOrdinal()
+        .domain(data)
+        .range(['blue', 'purple', 'maroon', '#FF7119'])
+
+    // Creating scales
+    const xscale = d3
+        .scaleLinear()
+        .range([0, scatterPlotInnerWidth - 20])
+        .domain([200, d3.max(data, (d) => +d.Likes)])
+
+    const yscale = d3
+        .scaleLinear()
+        .range([scatterPlotInnerHeight, 40])
+        .domain([0, d3.max(data, (d) => +d.followers)]);
+
+    // Creating axis'
+    const xaxis = d3.axisBottom().scale(xscale);
+    const yaxis = d3.axisLeft().scale(yscale);
+
+
+    const createXaxis = spG
+        .append("g")
+        .call(xaxis.tickFormat((d) => d / 1000000 + "m").tickSize(-scatterPlotInnerHeight + 40))
+        .attr("transform", `translate(0, ${scatterPlotInnerHeight})`)
+        .attr("color", "#c0c0c055")
+        .attr("style", "font-family: noto")
+
+    // add label to xaxis
+    createXaxis
+        .append("text")
+        .text("Likes")
+        .attr("class", "axis-name")
+        .attr("fill", "#fff")
+        .attr("x", 170)
+        .attr("y", 30)
+        .style("font-family", "noto")
+        .style("font-size", 15);
+
+
+    // create y axis    
+    const createYaxis = spG
+        .append("g")
+        .call(yaxis.tickFormat((d) => d / 100000 + "m").tickSize(-scatterPlotInnerWidth + 20))
+        .attr("color", "#c0c0c055")
+        .attr("style", "font-family: noto")
+
+
+    // add label to y axis
+    createYaxis
+        .append("text")
+        .text("Subscribers")
+        .attr("class", "axis-name")
+        .attr("fill", "#fff")
+        .style("transform", "rotate(-90deg)")
+        .attr("x", -150)
+        .attr("y", -45)
+        .style("font-family", "noto")
+        .style("font-size", 15);
+
+
+    // Populating the chart
+    spG
+        .selectAll("circle")
+        .data(data)
+        .join("circle")
+        .attr("r", 4)
+        .attr("cx", (d) => xscale(0))
+        .attr("cy", (d) => yscale(0))
+        .style("fill", d => color(d.Likes))
+        .style("opacity", 1)
+
+    spG.selectAll("circle")
+        .transition()
+        .duration(1000)
+        .attr("cx", (d) => xscale(d.Likes))
+        .attr("cy", (d) => yscale(d.followers))
+        .delay((d, i) => i * 10);
+
+    // add mouseover tool tip
+    spG.selectAll("circle")
+        .on("mouseover", function () {
+            //show the value of the ID and Flight_Distance when the mouse is over the data point
+            d3.select(this)
+                .transition()
+                .duration(100)
+                .attr("r", 8)
+
+            // Create a function to show the value of the data point on the top of the data point whenever the mouse is over the data point.
+            function showToolTip(d) {
+                console.log('d: ', d)
+                console.log(d.ChannelName)
+                return d.ChannelName;
+            }
+            //Show only the value of the data point when the mouse is over the data point
+            d3.select(this)
+                .append("title")
+                .text(showToolTip)
+            console.log("is this working?");
+        })
+
+        //when the mouse is out of the data point, the value will disappear
+        .on("mouseout", function () {
+            d3.select(this)
+                .transition()
+                .duration(1000)
+                .attr("r", 5)
+                .attr("fill", "#821214")
+                .attr("opacity", ".5");
+        });
+})
+
+//****************************************************** */
+// CHART 3 - Each Countries number of youtubers
+//****************************************************** */
+
+let svgChart3 = d3
+    .select("#chart2")
+    .append("svg")
+    .attr("width", "100%")
+    .attr("height", "100%")
+    .attr('preserveAspectRatio', 'xMinYMin meet')
+    .attr('viewBox', '0 -20 500 400')
+    .attr("style", "background-color: #2c2b2b")
+    .style("box-shadow", "0 0 15px 2px #c0c0c055")
+    .style("border-radius", "4px")
+
+
+let barChartG = svgChart3
+    .append("g")
+    .attr("class", "group")
+    .attr("transform", "translate(65, 10)")
+
+// add scatterplot title 
+spG
+    .append("text")
+    .attr("text-anchor", "center")
+    .attr("x", bInner_width / 8)
+    .attr("y", 0)
+    .text("Relationship Between Likes and Subs")
+    .style("fill", "#fff")
+    .style("font-size", 17)
+    .style("letter-spacing", 0.8)
+
+
+
 
 
 
